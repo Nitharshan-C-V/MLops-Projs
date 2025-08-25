6549a0da4773e4d0534fa090d9fe75d7ee7b103c
Nitharshan-C-V <nico8891@colorado.edu>
2025-08-25 15:55:29 -0500

    utils added

--- template.py
+++ template.py
@@ -13,7 +13,7 @@ list_of_files = [
     ".github/workflows/.gitkeep",
     f'src/{project_name}/__init__.py',
     f'src/{project_name}/components/__init__.py',
-    f'src{project_name}/utlis/__init__.py',
+    f'src/{project_name}/utlis/__init__.py',
     f'src/{project_name}/config/__init__.py',
     f'src/{project_name}/config/configuration.py',
     f'src/{project_name}/pipeline/__init__.py',
